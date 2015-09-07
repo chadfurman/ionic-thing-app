f007318596e8e1578de4b6f042a76104c88ad811
Chad Furman <chad@chadfurman.com>
2015-09-06 21:16:22 -0400

    added list controller and template

---
+++ app/main/controllers/list-ctrl.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('main')
+.controller('ListCtrl', function () {
+
+  console.log('Hello from your Controller: ListCtrl in module main:. This is your controller:', this);
+  // TODO: do your controller thing
+
+});
