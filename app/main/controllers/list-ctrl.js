afdbff02541e5c753509d7ba161ef93716060816
Chad Furman <chad@chadfurman.com>
2015-09-11 01:18:42 -0400

    attaching thigns to contrller scope

--- app/main/controllers/list-ctrl.js
+++ app/main/controllers/list-ctrl.js
@@ -3,10 +3,13 @@ angular.module('main')
 .controller('ListCtrl', function ($scope, Api) {
 
   console.log('Hello from your Controller: ListCtrl in module main:. This is your controller:', this);
-  // TODO: do your controller thing
 
+  var controller = this;
+
+  console.log('listing things');
   Api.listThings(function (things) {
-    $scope.things = things;
+    console.log('things: ', things);
+    controller.things = things.data;
   });
 
 });
